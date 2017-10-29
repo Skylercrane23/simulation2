179cdf5fba736e3a34dab17e2bb02ab23b0e15ec
Skyler Crane <skylercrane7@gmail.com>
2017-10-29 00:19:38 -0600

    added fontawesome to index.js folder so font-awesome is accessible everywhere

--- group-project/src/index.js
+++ group-project/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+
+import './css/main.css';
+
+// FONT AWESOME
+import 'font-awesome/css/font-awesome.min.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
