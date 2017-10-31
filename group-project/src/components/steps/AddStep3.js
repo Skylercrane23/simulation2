9fbca19e15187b4ef6e4a6435c3ad240e583d00e
Skyler Crane <skylercrane7@gmail.com>
2017-10-30 23:46:54 -0600

    Added steps components for 1-5 for routing purpsoes

---
+++ group-project/src/components/steps/AddStep3.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// COMPONENTS
+import Nav from './steps/Nav';
+
+class AddStep3 extends Component {
+  render() {
+    return (
+      <div className="AddStep3">
+        <Nav />
+        AddStep3 Component
+      </div>
+    );
+  }
+}
+
+export default AddStep3;
---
+++ group-project/src/components/steps/AddStep4.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// COMPONENTS
+import Nav from './steps/Nav';
+
+class AddStep4 extends Component {
+  render() {
+    return (
+      <div className="AddStep4">
+        <Nav />
+        AddStep4 Component
+      </div>
+    );
+  }
+}
+
+export default AddStep4;
