9ae44c871b3c9513a541036ec5834723c3f48587
Skyler Crane <skylercrane7@gmail.com>
2017-10-30 23:44:52 -0600

    Added AddProperty Default Component not functioning. just for routing purposes

---
+++ group-project/src/components/AddProperty.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddProperty extends Component {
+  render() {
+    return (
+      <div className="AddProperty">
+        AddProperty Component
+      </div>
+    );
+  }
+}
+
+export default AddProperty;
